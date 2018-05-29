f35475dbdc1c5f5f5fcc4c6d15f0db7de5f93acf
Stanley Kubasek <stanley@dstillery.com>
2018-05-29 08:49:13 -0400

    Update text

--- src/main/java/com/sk/hw_2/price/PairFinder.java
+++ src/main/java/com/sk/hw_2/price/PairFinder.java
@@ -7,8 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
-/**
- */
 public class PairFinder {
     private static class ItemPrice {
         String item;
